2c8b80d419f8efce1b57454318aadd05e0565d12
Fred'Dev <fredchenot@gmail.com>
2022-04-07 14:44:57 +0200

    feat: prestations design

--- src/components/Prestations/Prestations.jsx
+++ src/components/Prestations/Prestations.jsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './prestations.scss';
+import prestations from '../../data/prestations';
 
 function Prestations({ className, ...rest }) {
-  console.log('hello');
+  const handleClick = () => {
+    window.open('https://www.clicrdv.com/anita-chenot', '_blank');
+  };
   return (
     <div
       className={classnames('prestations', className)}
       {...rest}
     >
-      Hello prestations
+      {prestations.map((item) => (
+        <Card
+          sx={{ maxWidth: 345 }}
+          className="prestations-card"
+        >
+          <CardMedia
+            component="img"
+            alt={item.Title}
+            height="140"
+            image={item.UrlImg}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.Title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim accusantium, asperiores laboriosam necessitatibus unde molestiae totam corrupti, voluptatum reiciendis quisquam officia impedit vitae repellat possimus reprehenderit. Nihil incidunt optio natus!
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              className="prestations-link"
+              variant="contained"
+              color="secondary"
+              onClick={handleClick}
+            >
+              Prendre rendez-vous
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
     </div>
   );
 }
---
+++ src/data/prestations.js
@@ -0,0 +1,22 @@
+const prestations = [
+  {
+    Title: 'bilan init',
+    UrlImg: `https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_SERVER}/image/upload/v1649320094/fleur_ln0o6g.jpg`,
+    Description: 'loremdfsdfsdfsdfsdfsdf',
+    Price: '60 euros',
+  },
+  {
+    Title: 'bilan suivi',
+    UrlImg: `https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_SERVER}/image/upload/v1649320094/fleur_ln0o6g.jpg`,
+    Description: 'loremfsdsdfsffsdfsddfdfsdf',
+    Price: '40 euros',
+  },
+  {
+    Title: 'bilan sante',
+    UrlImg: `https://res.cloudinary.com/${process.env.REACT_APP_CLOUDINARY_SERVER}/image/upload/v1649320094/fleur_ln0o6g.jpg`,
+    Description: 'loremdssdfsfdsfaaaaaaaaaaaaaaa',
+    Price: '80 euros',
+  },
+
+];
+export default prestations;
