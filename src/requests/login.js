547bb3c3233e87298af4e0ef92038a2aea898be3
Fred'Dev <fredchenot@gmail.com>
2022-06-07 18:09:02 +0200

    feat: prepare back

---
+++ src/requests/login.js
@@ -0,0 +1,28 @@
+import apiAxios from './index';
+
+export async function requestLogin(user, pass) {
+  try {
+    const response = await apiAxios.post('/login', {
+      user,
+      pass,
+    });
+    return response;
+  }
+  catch (err) {
+    return err.response;
+  }
+}
+
+export async function requestCheck(token) {
+  try {
+    const response = await apiAxios.post('/login/check', {}, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
+    return response;
+  }
+  catch (err) {
+    return err.response;
+  }
+}
